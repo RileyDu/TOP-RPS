80a94e968f17d4f52aa939533c0cd79e05a84c5c
Riley DuBord <thelifeofdu@gmail.com>
2023-12-30 15:43:16 -0600

    a rps game function now works

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function getComputerChoice() {
   const randomNum = Math.floor(Math.random() * 3);
 
   // Options to choose from
-  const options = ["Rock", "Paper", "Scissors"];
+  const options = ["rock", "paper", "scissors"];
 
   // Choose the option based on the random number
   const chosenOption = options[randomNum];
@@ -12,8 +12,30 @@ function getComputerChoice() {
   return chosenOption;
 }
 
-function playRound(playerSelection, computerSelection) {}
+function playRound(playerSelection, computerSelection) {
+  
+  playerSelection = playerSelection.toLowerCase();
+    const choices = ["rock", "paper", "scissors"];
+    if (!choices.includes(playerSelection)){
+      return `Invalid choice. Please choose rock, paper, or scissors`;
+    }
+  if (playerSelection === computerSelection) {
+    return `issa tie boi`;
+  }
+
+  if (
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "paper" && computerSelection === "rock") ||
+    (playerSelection === "scissors" && computerSelection === "paper")
+  ) {
+    return `You win! ${playerSelection} beats ${computerSelection}.`
+  } else {
+    return `You lose! ${computerSelection} beats ${playerSelection}.`
+  }
+}
 
 const playerSelection = "rock";
 const computerSelection = getComputerChoice();
-console.log(computerSelection);
+console.log(`This is just outputting the random COM choice: ${computerSelection}`);
+console.log(`This is displaying the user input ${playerSelection}`)
+console.log(playRound(playerSelection, computerSelection));
